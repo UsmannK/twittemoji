c7e9fafce5708bd86968b3457a0dcdc75f810d13
Usmann Khan <usmann96@gmail.com>
2016-04-19 16:43:59 -0400

    Integrate Twitter

--- server/main.js
+++ server/main.js
@@ -1,1 +1,18 @@
 import '../imports/api/tasks.js';
+
+var Twitter = Meteor.npmRequire("twitter");
+var conf = JSON.parse(Assets.getText('twitter.json'));
+var twit = new Twitter({
+  consumer_key: conf.consumer.key,
+    consumer_secret: conf.consumer.secret,
+    access_token_key: conf.access_token.key,
+    access_token_secret: conf.access_token.secret
+});
+
+twit.stream('statuses/filter', {
+  'track': conf.hashtag
+}, function(stream) {
+  stream.on('data', function(data) {
+    console.log(data);
+  });
+});
