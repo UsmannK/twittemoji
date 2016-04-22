8dbe34eccb24a10c10a8c7b7a30f58b22a3d359a
Nicky Semenza <nicky@nickysemenza.com>
2016-04-21 22:25:27 -0400

    test bar graph

--- client/main.js
+++ client/main.js
@@ -3,12 +3,14 @@ import angularMeteor from 'angular-meteor';
 import tweetsList from '../imports/components/tweetsList/tweetsList';
 import 'angular-google-maps';
 import 'angular-simple-logger';
+import 'angular-chart.js';
 
 angular.module('simple-tweets', [
   angularMeteor,
   tweetsList.name,
   'nemLogging',
-  'uiGmapgoogle-maps'
+  'uiGmapgoogle-maps',
+  'chart.js'
 ]);
 
 
--- imports/components/tweetsList/tweetsList.js
+++ imports/components/tweetsList/tweetsList.js
@@ -8,6 +8,18 @@ class TweetsListCtrl {
 
   constructor($scope) {
 
+
+    // $scope.opts = {scaleBeginAtZero:false};
+
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [65, -59, 80, 81, 56, -55, 40],
+    [28, 48, -40, 19, 86, 27, 90]
+  ];
+
+
     $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 2 };
 
 
